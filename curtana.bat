d66c5012e994e8fae720d34006e26b3b04f8e118
yaytsa-boga <55556801+yaytsa-boga@users.noreply.github.com>
2022-04-09 23:55:41 +0500

    Delete curtana.bat

--- curtana.bat
+++
@@ -1,4 +0,0 @@
-rem This file has instrutions for downloading a suitable recovery for curtana devices.
-rem Recovery is TWRP 3.6.0_11 FBE v2 UNOFFICIAL by meganukebmp
-rem This is the only one recovery that properly responds to command "twrp sideload"
-curl 
