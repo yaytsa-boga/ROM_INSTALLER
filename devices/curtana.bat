f835a547253f666695c239bc4139424a2bad7c76
yaytsa-boga <55556801+yaytsa-boga@users.noreply.github.com>
2022-04-10 00:27:31 +0500

    Update curtana.bat

--- devices/curtana.bat
+++ devices/curtana.bat
@@ -1,4 +1,9 @@
 rem This file has instrutions for downloading a suitable recovery for curtana devices.
 rem Recovery is TWRP 3.6.0_11 FBE v2 UNOFFICIAL by meganukebmp
 rem This is the only one recovery that properly responds to command "twrp sideload"
-curl 
+curl https://drive.google.com/uc?export=download&id=1b1YQuZcc8gWUfaHwDUZrULJE8OSVq6Qc
+curl https://drive.google.com/uc?export=download&id=15yTYuMU5oR5Rh_dhwI0EASIF6ZrtQH__
+
+
+https://drive.google.com/file/d/1b1YQuZcc8gWUfaHwDUZrULJE8OSVq6Qc/view?usp=sharing
+https://drive.google.com/file/d/15yTYuMU5oR5Rh_dhwI0EASIF6ZrtQH__/view?usp=sharing
