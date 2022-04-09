1f1dbafac310143cec131f2a6c210ff2b126cae0
yaytsa-boga <55556801+yaytsa-boga@users.noreply.github.com>
2022-04-10 01:04:44 +0500

    Update curtana.bat

--- devices/curtana.bat
+++ devices/curtana.bat
@@ -4,7 +4,7 @@ rem This is the only one recovery that properly responds to command "twrp sidelo
 rem "cd %~dp0..\..\..\" - returns the programm to the root folder of the programm
 cd %~dp0..\..\..\
 mkdir %cd%\compatibility\temp
-%cd%\curl\curl "https://drive.google.com/uc?export=download&id=1b1YQuZcc8gWUfaHwDUZrULJE8OSVq6Qc" -o %cd%\compatibility\temp\curtana.7z.001
-%cd%\curl\curl "https://drive.google.com/uc?export=download&id=15yTYuMU5oR5Rh_dhwI0EASIF6ZrtQH__" -o %cd%\compatibility\temp\curtana.7z.002
+%cd%\curl\curl -L "https://drive.google.com/uc?export=download&id=1b1YQuZcc8gWUfaHwDUZrULJE8OSVq6Qc" -o %cd%\compatibility\temp\curtana.7z.001
+%cd%\curl\curl -L "https://drive.google.com/uc?export=download&id=15yTYuMU5oR5Rh_dhwI0EASIF6ZrtQH__" -o %cd%\compatibility\temp\curtana.7z.002
 %cd%\7zip\7z  x %cd%\compatibility\temp\curtana.7z.001  -o%cd%\recovery\curtana.img
 rd /s /q %cd%\compatibility\temp
