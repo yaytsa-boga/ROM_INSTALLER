9135e2884bf2afe2aaf27b4acf56833b72d372f8
yaytsa-boga <55556801+yaytsa-boga@users.noreply.github.com>
2022-04-10 01:08:43 +0500

    Update curtana.bat

--- devices/curtana.bat
+++ devices/curtana.bat
@@ -4,7 +4,7 @@ rem This is the only recovery that properly responds to command "twrp sideload"
 rem "cd %~dp0..\..\..\" - returns the programm to the root folder of the programm
 cd %~dp0..\..\..\
 mkdir %cd%\compatibility\temp
-%cd%\curl\curl -L "https://drive.google.com/uc?export=download&id=1b1YQuZcc8gWUfaHwDUZrULJE8OSVq6Qc" -o %cd%\compatibility\temp\curtana.7z.001
-%cd%\curl\curl -L "https://drive.google.com/uc?export=download&id=15yTYuMU5oR5Rh_dhwI0EASIF6ZrtQH__" -o %cd%\compatibility\temp\curtana.7z.002
-%cd%\7zip\7z  x %cd%\compatibility\temp\curtana.7z.001  -o%cd%\recovery\curtana.img
+%cd%\curl\curl -L "https://drive.google.com/uc?export=download&id=1b1YQuZcc8gWUfaHwDUZrULJE8OSVq6Qc" -o %cd%\compatibility\temp\%model%.7z.001
+%cd%\curl\curl -L "https://drive.google.com/uc?export=download&id=15yTYuMU5oR5Rh_dhwI0EASIF6ZrtQH__" -o %cd%\compatibility\temp\%model%.7z.002
+%cd%\7zip\7z  x %cd%\compatibility\temp\curtana.7z.001  -o%cd%\recovery
 rd /s /q %cd%\compatibility\temp
